ed4ec0a6ac330b2118f1d31049d00d93a8260e5b
ynotstartups <ynotstartups@gmail.com>
2025-07-18 08:57:56 +0100

    learning os

---
+++ Makefile
@@ -0,0 +1,5 @@
+run-p1-fork:
+	python p1-fork.py
+
+run-watch-ps:
+	watch ps -j
---
+++ p1-fork.py
@@ -0,0 +1,29 @@
+import os
+from time import sleep
+import urllib.request
+
+def get(url: str):
+    with urllib.request.urlopen('http://python.org/') as response:
+       html = response.read()
+    return html
+
+print(f"Hello world {os.getpid()}")
+result_number = os.fork()
+
+if result_number < 0:
+    print('fork failed')
+    exit(1)
+elif result_number == 0:
+    print(f"hello, I am child with process id {os.getpid()}")
+    # child new process
+    # sleep(10)
+    # html = get('https://httpbin.org/delay/10')
+    os.system('bash test.curl')
+    # print(f"downloaded html {html[:100]}")
+    # print(html)
+else:
+    # sleep(5)
+    # parent is here
+    print(f"hello, I am parent of {result_number} with process id {os.getpid()}")
+
+
